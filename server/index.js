3244c0f3a4126d1991bd66eacbd8d739afdafeae
Dhruvvats13 <78378557+Dhruvvats13@users.noreply.github.com>
2023-04-05 23:13:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -90,6 +90,48 @@ app.post('/login', async(req,res)=>{
         success:true,
     })
 })
+app.post("/forgot-password", async (req, res) => {
+    const { email } = req.body;
+  
+    // TODO: Validate email address
+  
+    // Generate a password reset token (e.g. using JWT)
+  
+    // Send the password reset email
+    try {
+      const transporter = nodemailer.createTransport({
+        // Replace with your SMTP server settings
+        host: "smtp.example.com",
+        port: 587,
+        secure: false,
+        auth: {
+          email: "your_email",
+          pass: "your_password",
+        },
+      });
+  
+      const mailOptions = {
+        from: "noreply@example.com",
+        to: email,
+        subject: "Reset Your Password",
+        html: `
+          <p>Hi there,</p>
+          <p>Please click the following link to reset your password:</p>
+          <p><a href="https://yourwebsite.com/reset-password?token=${resetToken}">Reset Password</a></p>
+          <p>If you didn't request a password reset, please ignore this email.</p>
+        `,
+      };
+  
+      await transporter.sendMail(mailOptions);
+  
+      res.status(200).json({
+        message: `An email with instructions to reset your password has been sent to ${email}.`,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "An error occurred. Please try again later." });
+    }
+  });
 app.listen(3001,()=>{
     console.log('server running on port no 3000')
 
